15ffb67d8352b10716208121d0087efdad9e07bc
Salim Mansour <Salim.Mansour@camh.ca>
2019-08-29 12:43:20 -0400

    Modified single synb0 gen script

--- src/gen_synb0_single.py
+++ src/gen_synb0_single.py
@@ -3,22 +3,29 @@ import sys
 from synb0 import synb0
 from shutil import rmtree, copy
 
-in_file = sys.argv[1]
-root = sys.argv[2]
-output_folder = sys.argv[3]
+def gen_synb0_single(in_file, root, output_folder):
+    print("gen single synb0")
+    # Generate tmp_folder
+    temp_folder = os.path.join(output_folder, "synb0_tmp")
 
-# Generate tmp_folder
-temp_folder = os.path.join(output_folder, "synb0_tmp")
+    # Grab the base name of the file
+    name = (os.path.basename(in_file)).split("_T1")[0]
 
-# Grab the base name of the file
-name = (os.path.basename(in_file)).split("_T1")[0]
+    # Call the function to generate the Synb0
+    output_file = synb0(temp_folder, in_file, name, root)
 
-# Call the function to generate the Synb0
-output_file = synb0(temp_folder, in_file, name, root)
-print(output_file)
+    # Copy the synb0 out of the temp file
+    copy(output_file, output_folder)
 
-# Copy the synb0 out of the temp file
-copy(output_file, output_folder)
+    # Clear all other files
+    rmtree(temp_folder)
 
-# Clear all other files
-rmtree(temp_folder)
+def main():
+    in_file = sys.argv[1]
+    synb0_root = sys.argv[2]
+    output_folder = sys.argv[3]
+
+    gen_synb0_single(in_file, synb0_root, output_folder)
+
+if __name__== "__main__":
+    main()
